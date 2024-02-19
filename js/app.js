12f296f9c931f8b236674009b2f81ca158dbb2e4
Peter Aluya <aluyapeter7@gmail.com>
2024-02-19 08:52:56 +0100

    rounding up

--- js/app.js
+++ js/app.js
@@ -1,18 +1,16 @@
 
-function validateEmail() {
+ function validateEmail() {
     var emailInput = document.getElementById('email');
     var errorMessage = document.getElementById('error-message');
     var emailValue = emailInput.value;
 
-    // Basic email validation using a regular expression
     var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     if (!emailRegex.test(emailValue)) {
-        errorMessage.style.display = 'block'; // Display error message
-        return false; // Prevent form submission
+        errorMessage.style.display = 'block'; 
+        return false;
     } else {
-        errorMessage.style.display = 'none'; // Hide error message
-        // You can perform additional actions here before submitting the form
-        return true; // Allow form submission
+        errorMessage.style.display = 'none';
+        return true;
     }
 }
